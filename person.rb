8cda54e925e9ee3a5a79e605658279529e0880e1
oscarfmdev <oscarfmdev@gmail.com>
2022-12-20 12:08:39 -0600

    Add person class

---
+++ person.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+# Person class
+class Person
+  def initialize(age, name = 'Unknown', parent_permission = true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  private def of_age?
+    @age >= 18
+  end
+end
+
+person1 = Person.new(18, 'Juan', false)
+
+puts person1.can_use_services? # true
