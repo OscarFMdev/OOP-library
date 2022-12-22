6b8863e22def6406642a5f1004a6eafc3f7e099e
Oscar Fernández Muñoz <oscarfmdev@gmail.com>
2022-12-22 11:54:09 -0600

    Add console outputs

--- person.rb
+++ person.rb
@@ -35,12 +35,12 @@ puts person1.can_use_services? # true
 
 person = Person.new(22, 'maximilianus')
 
-puts person.correct_name
+puts person.correct_name # 'maximilianus'
 
 capitalizedPerson = CapitalizeDecorator.new(person)
 
-puts capitalizedPerson.correct_name
+puts capitalizedPerson.correct_name # 'Maximilianus'
 
 capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
 
-puts capitalizedTrimmedPerson.correct_name
+puts capitalizedTrimmedPerson.correct_name # 'Maximilianus'
