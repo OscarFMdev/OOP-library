0f01e316982ec28e2e03e3670fd4babb6cc54675
Oscar Fernández Muñoz <oscarfmdev@gmail.com>
2022-12-22 11:52:11 -0600

    Edit trimmer decorator

--- trimmer-decorator.rb
+++ trimmer-decorator.rb
@@ -2,6 +2,6 @@ require './decorator'
 # Capitalize decorator class
 class TrimmerDecorator < Decorator
   def correct_name
-    @nameable.correct_name.trim
+    @nameable.correct_name.strip
   end
 end
