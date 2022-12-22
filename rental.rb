fc9a0826dbdb9caae9ad2d73fa183722805f66f3
Oscar Fernández Muñoz <oscarfmdev@gmail.com>
2022-12-22 15:14:55 -0600

    Add person and book to rental

--- rental.rb
+++ rental.rb
@@ -1,6 +1,11 @@
+require './book'
+require './person'
 class Rental
-  attr_accessor :date
-  def initialize(date)
+  attr_accessor :date, :book, :person
+  def initialize(date, person)
     @date = date
+    @person = person
+    book.rentals << self
+    person.rentals << self
   end
 end
