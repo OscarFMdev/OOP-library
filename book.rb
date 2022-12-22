92f73b965f46ff292620af1766f3f1d07525fa8c
Oscar Fernández Muñoz <oscarfmdev@gmail.com>
2022-12-22 14:57:53 -0600

    Add rental to book class

--- book.rb
+++ book.rb
@@ -1,7 +1,13 @@
+require './rental'
 class Book
   attr_accessor :title, :author
   def initialize(title, author)
     @title = title
     @author = author
+    @rentals = []
+  end
+
+  def add_rental(date)
+    @rentals.push(date, @title, @author)
   end
 end
